422a4af126ca35e9c9f52f88bec4c37656e505ae
ThinhHoang2310 <thinhhoangdinh123@gmail.com>
2025-08-14 15:05:54 +0700

    change Ourshop

--- src/pages/OurShop/OurShop.jsx
+++ src/pages/OurShop/OurShop.jsx
@@ -3,7 +3,7 @@ import MainLayout from '@components/Layout/Layout';
 import styles from './styles.module.scss';
 import { useNavigate } from 'react-router-dom';
 import Banner from '@pages/OurShop/components/Banner';
-import { OurShopProvider, OurShopContext } from '@contexts/ourShopProvider';
+import { OurShopProvider, OurShopContext } from '@contexts/OurShopProvider';
 import { useContext } from 'react';
 import Filter from '@pages/OurShop/components/Filter';
 import ListProducts from '@pages/OurShop/components/ListProducts';
@@ -35,10 +35,10 @@ function OurShop() {
             <Banner />
             <div>
                <Filter />
-               <ListProducts/>
+               <ListProducts />
             </div>
          </MainLayout>
-         <MyFooter/>
+         <MyFooter />
       </OurShopProvider>
    );
 }
