cb0894210bc56cc96b06c8fea3c307696621caae
Đinh Hoàng Thịnh <thinhhoangdinh123@gmail.com>
2025-09-10 18:34:55 +0700

    update-code

--- src/pages/DetailProduct/components/FormItem.jsx
+++ src/pages/DetailProduct/components/FormItem.jsx
@@ -2,10 +2,26 @@ import { FaStar } from 'react-icons/fa';
 import styles from '../styles.module.scss';
 
 function FormItem({ label, isRequired, typeChildren }) {
+   const { formItem,boxItemStar } = styles;
+
+   const renderStar = length => {
+      return Array.from({ length }, (_, index) => (
+         <FaStar key={index} style={{ color: '#e1e1e1' }} />
+      ));
+   };
+
    const renderChildren = () => {
       switch (typeChildren) {
          case 'rating':
-            return <FaStar />;
+            return (
+               <div className={boxItemStar}>
+                  <div>{renderStar(1)}</div>
+                  <div>{renderStar(2)}</div>
+                  <div>{renderStar(3)}</div>
+                  <div>{renderStar(4)}</div>
+                  <div>{renderStar(5)}</div>
+               </div>
+            );
          case 'input':
             return <input type="text" />;
          case 'textarea':
@@ -14,14 +30,13 @@ function FormItem({ label, isRequired, typeChildren }) {
    };
 
    return (
-      <>
+      <div className={formItem}>
          <label>
             {label} {isRequired && <span>*</span>}
          </label>
          {renderChildren()}
-      </>
+      </div>
    );
 }
-weeerw
 
 export default FormItem;
--- src/pages/DetailProduct/components/Review.jsx
+++ src/pages/DetailProduct/components/Review.jsx
@@ -1,53 +1,76 @@
 import Button from '@components/Button/Button';
 import FormItem from '@/pages/DetailProduct/components/FormItem';
 
+import styles from '../styles.module.scss';
+
 function ReviewProduct() {
+   const {
+      containerReview,
+      reviews,
+      noreview,
+      replyForm,
+      commentReplyTitle,
+      commentNotes,
+      commentFormCookiesConsent,
+      btnSubmit
+   } = styles;
+
    return (
-      <div>
-         <div>REVIEWS</div>
-         <p>There are no reviews for this product.</p>
+      <div className={containerReview}>
+         <div className={reviews}>REVIEWS</div>
+         <p className={noreview}>There are no reviews for this product.</p>
 
-         <div>BE THE FIRST TO WRITE A REVIEW *10K*</div>
-         <p>
-            Your email address will not be published. Required fields are marked
-         </p>
-
-         {/* RATING  */}
-         <FormItem
-            label={'Your rating'}
-            isRequired={true}
-            typeChildren="rating"
-         />
-
-         {/* AREA */}
-         <FormItem
-            label={'Your review'}
-            isRequired={true}
-            typeChildren="textarea"
-         />
-
-         {/* NAME  */}
-         <FormItem 
-         label={'Name '}
-          isRequired={true}
-           typeChildren="input" />
-
-         {/* EMAIL  */}
-         <FormItem
-            label={'Email'}
-            isRequired={true}
-            typeChildren="input"
-         />
-
-         <div>
-            <input type="checkbox" />
-            <p>
-               Save my name,email,and website in this browser for the next time
-               I comment
+         <div className={replyForm}>
+            <div className={commentReplyTitle}>
+               BE THE FIRST TO WRITE A REVIEW *10K*
+            </div>
+            <p className={commentNotes}>
+               Your email address will not be published. Required fields are
+               marked
             </p>
-         </div>
 
-         <Button content="Submit" />
+            <form action="">
+               {/* RATING  */}
+               <FormItem
+                  label={'Your rating'}
+                  isRequired={true}
+                  typeChildren="rating"
+               />
+
+               {/* AREA */}
+               <FormItem
+                  label={'Your review'}
+                  isRequired={true}
+                  typeChildren="textarea"
+               />
+
+               {/* NAME  */}
+               <FormItem
+                  label={'Name '}
+                  isRequired={true}
+                  typeChildren="input"
+               />
+
+               {/* EMAIL  */}
+               <FormItem
+                  label={'Email'}
+                  isRequired={true}
+                  typeChildren="input"
+               />
+
+               <div className={commentFormCookiesConsent}>
+                  <input type="checkbox" />
+                  <span>
+                     Save my name,email,and website in this browser for the next
+                     time I comment
+                  </span>
+               </div>
+
+               <div className={btnSubmit}>
+                  <Button content="Submit" />
+               </div>
+            </form>
+         </div>
       </div>
    );
 }
